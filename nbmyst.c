a5b318b81ebfcbd03eba84eb2a5e34d74b7a2098
tfhazerty <64971565+tfhazerty@users.noreply.github.com>
2021-01-25 16:37:56 +0100

    Update nbmyst.c

--- nbmyst.c
+++ nbmyst.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
+// Library C 
 int main(void){
-    const int max = 100, min = 1;
+    const int max = 100, min = 1; // Declaration and assignment of a variable of type int which is constant
     //
-    int nbjeu;
+    int nbjeu; //Declaration of a variable 
     //
     srand(time(NULL));
-    int nombreMystere = (rand() % (max  - min + 1))+ min;
+    int nombreMystere = (rand() % (max  - min + 1))+ min; //Declaration of a variable that generates a random number 
     //
     printf("Votre nombre a etait generer !\n");
     scanf("%d", &nbjeu);
@@ -20,7 +20,7 @@ int main(void){
         printf("C'est plus\n");
         scanf("%d", &nbjeu);
     }
-    else if (nbjeu > nombreMystere)
+    else if (nbjeu > nombreMystere) //the game made with a while loop and the conditions
     {
         printf("C'est moins\n");
         scanf("%d", &nbjeu);
@@ -30,6 +30,7 @@ int main(void){
     {
         printf("Bravo, tu as gagne !");
     }
+    
     /*printf("Voulez vous rejouez ? \n Tapez 1 pour rejouez.\n")*/
     return 0;
 }
