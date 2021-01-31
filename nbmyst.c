783b2072d46745b0c2db3963f581773e772801c6
tfhazerty <64971565+tfhazerty@users.noreply.github.com>
2021-01-31 10:34:11 +0100

    Update nbmyst.c

--- nbmyst.c
+++ nbmyst.c
@@ -1,36 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-// Library C 
-int main(void){
-    const int max = 100, min = 1; // Declaration and assignment of a variable of type int which is constant
+
+int main(void) {
+    const int max = 100, min = 1;
     //
-    int nbjeu; //Declaration of a variable 
+    int nbjeu;
+    int nbjeu2; 
+    int choixMenu;
     //
     srand(time(NULL));
-    int nombreMystere = (rand() % (max  - min + 1))+ min; //Declaration of a variable that generates a random number 
+    int nombreMystere = (rand() % (max  - min + 1))+ min;
     //
-    printf("Votre nombre a etait generer !\n");
-    scanf("%d", &nbjeu);
-
-    while (nbjeu != nombreMystere)
+    printf("<---------MENU--------->\n");
+    printf("1. Mode 1 joueur.\n");
+    printf("2. Mode 2 joueurs.\n\n");
+    printf("3. Quitter\n");
+    scanf("%d", &choixMenu);
+    ///////////
+    switch(choixMenu){
+        case 1:
+        printf("----------------NIVEAUX--------------------");
+        printf("1. Niveau 1 -- 0 a 100");
+        printf("1. Niveau 1 -- 0 a 1000");
+        printf("1. Niveau 1 -- 0 a 10000");
+    break;
+    /////////////
+    case 2:
+    
+        printf("Bienvenu dans le mode 2 joueurs !\nUn des deux joueurs est obliger de saisir un nombre pour que le second le devienne\n");
+        scanf("%d", &nbjeu2);
+        system("cls"); 
+        printf("\nMaintenant au second joueur de devinnez !!! x)\n");
+        scanf("%d", &nbjeu);
+        while (nbjeu != nbjeu2)
     {
-        if (nbjeu < nombreMystere)
+        if (nbjeu < nbjeu2)
     {
         printf("C'est plus\n");
         scanf("%d", &nbjeu);
     }
-    else if (nbjeu > nombreMystere) //the game made with a while loop and the conditions
+    else if (nbjeu > nbjeu2)
     {
         printf("C'est moins\n");
         scanf("%d", &nbjeu);
     }
     }
-    if (nbjeu == nombreMystere)
+    if (nbjeu == nbjeu2)
     {
         printf("Bravo, tu as gagne !");
     }
+    break;
+
+    ///////////
+    case 3:
+        printf("A la prochaine !\n");
+        return 0;
+        break;
+
+    }
     
-    /*printf("Voulez vous rejouez ? \n Tapez 1 pour rejouez.\n")*/
+    
+    getc(5000);
     return 0;
 }
